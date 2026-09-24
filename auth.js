aed2afcadca55a560fe0d34abcd4da55368565f1
Keshawn <kbarbary24@gmail.com>
2026-09-24 06:29:05 -0400

    fix: connect to MongoDB lazily so builds don't require MONGO_URI

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
 import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/libs/mongo";
+import getMongoClient from "@/libs/mongo";
 
 const config = {
   providers: [
@@ -16,7 +16,7 @@ const config = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(getMongoClient),
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
